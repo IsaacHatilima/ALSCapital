475039a4fa641921d2707713fe0d299001ab4bfc
Isaac Hatilima <31442614+IsaacHatilima@users.noreply.github.com>
2021-05-28 05:06:56 +0200

    ENV File

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@fyflkt81z)u$v_8h1en+!ezdjje53eg1n-&e@*b8%^f&=@x07'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -82,13 +86,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'techzm_asl_capital',
-        # 'NAME': 'als_webapp',
-        'USER' : 'techzm_demo',
-        # 'USER' : 'admin',
-        'PASSWORD' : 'jlzn@h4mT^,0',
-        # 'PASSWORD' : 'Password123$$',
-        'HOST' : 'localhost',
+        # 'NAME': 'techzm_asl_capital',
+        'NAME': os.environ['NAME'],
+        # 'USER' : 'techzm_demo',
+        'USER' : os.environ['USER'],
+        # 'PASSWORD' : 'jlzn@h4mT^,0',
+        'PASSWORD' : os.environ['PASSWORD'],
+        # 'HOST' : 'localhost',
         'PORT' : '3306',
     }
 }
